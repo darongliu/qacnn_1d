a4e677ec0621ef2bae9eef1d0806dcdf82630afa
darongliu <b01901079@ntu.edu.tw>
2018-10-03 12:50:00 +0800

    minored

--- run.sh
+++ run.sh
@@ -5,11 +5,15 @@ mode='train'
 train_data='/home/darong/darong/qacnn_1d/data/movie_qa/train.json'
 dev_data='/home/darong/darong/qacnn_1d/data/movie_qa/dev.json'
 
+dropout=0.1
+cnn_layers=4
+
 resume_dir=''
 save_dir='./model/movie_qa'
 log='./log/movie_wa_log'
 test_result='./result'
 
 CUDA_VISIBLE_DEVICES=$gpu_id python main.py $mode --train_data $train_data --dev_data $dev_data \
+--dropout $dropout --cnn_layers $cnn_layers \
 --save_dir $save_dir
 

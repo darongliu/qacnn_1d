b056a9ce2de589ac01adf78698beb8d6f91f982a
r06944010 <kgb@m40.hao>
2018-10-04 18:17:24 +0800

    minored

--- run.sh
+++ run.sh
@@ -5,12 +5,11 @@ mode='train'
 train_data='/home/kgb/qacnn_1d/data/movie_qa/train.json'
 dev_data='/home/kgb/qacnn_1d/data/movie_qa/dev.json'
 
-dropout=0.1
-cnn_layers=4
+dropout=0.2
+cnn_layers=3
 
 #resume_dir=''
 save_dir='./model/movie_qa'
-log='movie_qa_log'
 test_result='./result'
 
 CUDA_VISIBLE_DEVICES=$gpu_id python main.py $mode --train_data $train_data --dev_data $dev_data \

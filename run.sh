70d87ec3bd740170eb579a014c17f653c22ffc34
r06944010 <kgb@m40.hao>
2018-10-11 18:17:05 +0800

    minored

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-gpu_id=3
+gpu_id=0
 
 mode='train'
 batch_size=32
@@ -12,7 +12,7 @@ dropout=0.1
 cnn_layers=4
 
 #resume_dir='./model/movie_qa'
-save_dir='./model/test'
+save_dir='./model/feat_aug'
 test_result='./result'
 
 CUDA_VISIBLE_DEVICES=$gpu_id python main.py $mode --batch_size $batch_size --train_data $train_data --dev_data $dev_data \

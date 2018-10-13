c7e0eb86b31a751aa4e2540bd2bd2980cbd7a381
r06944010 <kgb@m40.hao>
2018-10-13 10:23:54 +0800

    minored

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-gpu_id=0
+gpu_id=3
 
 mode='train'
 batch_size=32
@@ -12,7 +12,7 @@ dropout=0.1
 cnn_layers=4
 
 #resume_dir='./model/movie_qa'
-save_dir='./model/test_final'
+save_dir='./model/test_final_4'
 test_result='./result'
 
 CUDA_VISIBLE_DEVICES=$gpu_id python main.py $mode --batch_size $batch_size --train_data $train_data --dev_data $dev_data \
--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
-gpu_id=0
+gpu_id=1
 
 mode='test'
 batch_size=32
@@ -9,7 +9,7 @@ dropout=0.
 cnn_layers=4
 
 #resume_dir='./model/movie_qa'
-resume_dir='./model/test_1'
+resume_dir='./model/test_final'
 test_result='./result'
 
 CUDA_VISIBLE_DEVICES=$gpu_id python main.py $mode --batch_size $batch_size --test_data $test_data \
